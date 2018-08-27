7249c20b965f76e449dd78824024e4ea1e6e899d
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-27 20:19:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function lowerCaseDrivers(drivers) {
 
 function nameToAttributes(drivers) {
   const newDrivers = drivers.map(function(d) { return Object.assign({}, d, { firstName: d.split[1] })
-  })
+  });
   return newDrivers;
 }
