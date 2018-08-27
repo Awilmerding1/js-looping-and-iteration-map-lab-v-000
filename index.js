d7c90e7eefffe2f7f2f5549d04d5faae67a24db9
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-27 20:07:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers) {
-  const newDrivers = drivers.map(function(d) {})
+  const newDrivers = drivers.map(function(d) {return d.toUpperCase})
 }
